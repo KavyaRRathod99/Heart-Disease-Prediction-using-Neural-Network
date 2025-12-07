ae8dfcd77936f5501271168f3c2d40e88667f789
KavyaRRathod99 <167070255+KavyaRRathod99@users.noreply.github.com>
2025-12-07 11:47:16 +0530

    Add necessary imports for project setup

---
+++ Project_Setup_and_Imports.py
@@ -0,0 +1,10 @@
+import kagglehub
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from tensorflow.keras import layers, models
